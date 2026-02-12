f73149e048e92209e4a40336f7fa913a7b2a320c
ErikaGarciaR <garciarendon.eri@gmail.com>
2026-02-12 13:24:06 -0600

    Modificacion de list.f de excelium

--- verif/work_excelium/list.f
+++ verif/work_excelium/list.f
@@ -1,15 +1,14 @@
-../monitoreo_pkg.sv
-../interface_monitoreo.sv
 
+../property_defines.svh
 # ============================ rtl
   ../../rtl/monitoreo_top.sv
   ../../rtl/estado_temp.sv
   ../../rtl/comparador_temp.sv
   ../../rtl/registro_temp.sv
-
 # ============================ verificación
 
 ../aserciones_monitoreo.sv
-  
+../monitoreo_pkg.sv
+../interface_monitoreo.sv
 # =========================== tb
   ../monitoreo_tb.sv
